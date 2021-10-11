835092f9e34feaf8a8fbc7781bfc833e119c3e19
samroodAli <devsamrood@gmail.com>
2021-10-11 12:03:18 +0530

    State:UseState type with angle brackets or inference

--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+
 const GuestList: React.FC = () => {
+  const [name, setName] = useState("");
+  const [guestList, setGuestList] = useState<string[]>([]);
+
+  const onClick = () => {
+    setGuestList([...guestList, name]);
+  };
+
   return (
     <div>
-      <input />
-      <button>Add Guest</button>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={onClick}>Add Guest</button>
+      <ul>
+        {guestList.map((guest) => (
+          <li key={guest}>{guest}</li>
+        ))}
+      </ul>
     </div>
   );
 };
